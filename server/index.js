1dd582fe5711805ca6704dccd402f32e89110918
TheMangoMaster <themangomaster.official@gmail.com>
2025-05-17 17:47:50 -0400

    Updated CORS Access.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,16 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://job-matcher-steel.vercel.app"
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api", require("./routes"));
