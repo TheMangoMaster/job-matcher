d3c80b3386adcb573eac77405baf276eeba4c426
TheMangoMaster <themangomaster.official@gmail.com>
2025-05-17 17:26:14 -0400

    Updated port in index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,6 @@ app.use(express.json());
 
 app.use("/api", require("./routes"));
 
-app.listen(PORT, () => {
-  console.log(`🌐 Server running on http://localhost:${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running...");
 });
